de173ba3e77060fcd97c04728b07367fdbd52a89
Clay Grant <clayhgrant@gmail.com>
2024-12-15 15:40:51 -0500

    program.cs fixes

--- CodeLouCapstone/Program.cs
+++ CodeLouCapstone/Program.cs
@@ -7,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 //.AddInteractiveWebAssemblyComponents();
-builder.Services.AddHttpClient<IDeckService, DeckService>(client => client.BaseAddress = new Uri("https://localhost:7226/"));
-builder.Services.AddHttpClient<ICardService, CardService>(client => client.BaseAddress = new Uri("https://localhost:7226/"));
+builder.Services.AddHttpClient<IDeckService, DeckService>(client => client.BaseAddress = new Uri("http://localhost:5265/"));
+builder.Services.AddHttpClient<ICardService, CardService>(client => client.BaseAddress = new Uri("http://localhost:5265/"));
 
 var app = builder.Build();
 
